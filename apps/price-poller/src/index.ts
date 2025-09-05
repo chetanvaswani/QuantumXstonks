2b1c1086b3c5dea41474b2fdf27e16ef054ce006
chetanvaswani <chetan.vaswani.cv@gmail.com>
2025-09-05 22:36:52 +0530

    init and price-poller

---
+++ apps/price-poller/src/index.ts
@@ -0,0 +1,67 @@
+import WebSocket from "ws"
+import { createClient } from "redis";
+
+const publisher = createClient({
+    url: "redis://localhost:6379"
+});
+
+async function connectRedis(){
+    await publisher.connect()
+}
+connectRedis()
+
+const ws = new WebSocket('wss://ws.backpack.exchange')
+
+const DECIMAL_PRECISION = 4;
+interface assetPriceInterface{
+    [asset: string]: {
+        price: number,
+        decimal: number,
+        timeStamp: number
+    }
+}
+
+const ALL_ASSETS = ["bookTicker.BTC_USDC", "bookTicker.SOL_USDC", "bookTicker.ETH_USDC"];
+
+const currPrices: assetPriceInterface = {
+    ...Object.fromEntries(ALL_ASSETS.map((asset) => {
+        return [asset, {
+            price: 0,
+            decimal: DECIMAL_PRECISION,
+            timeStamp: (Date.now() * 1000)
+        }]
+    }))
+}
+
+setInterval(() => {
+    ALL_ASSETS.forEach((asset) => {
+        if (currPrices[asset]?.price === 0) {
+            return
+        }
+        publisher.xAdd(asset, "*", {
+            message: JSON.stringify(currPrices[asset])
+        })
+        console.log(asset, currPrices[asset], "pushed to stream")
+    })
+}, 100)
+
+ws.onopen = () => {
+    ALL_ASSETS.forEach((asset) => {
+        const subscription = {
+            "method": "SUBSCRIBE",
+            "params": [`${asset}`]
+        }
+    
+        ws.send(JSON.stringify(subscription))
+    })
+}
+
+ws.on("message", async (data) => {
+    const res = JSON.parse(data.toString())
+    // console.log(res)
+    currPrices[res.stream] = {
+        price: parseInt((Number(res.data.a) * (10 ** DECIMAL_PRECISION)).toFixed(0)),
+        decimal: DECIMAL_PRECISION,
+        timeStamp: res.data.T
+    }
+})
